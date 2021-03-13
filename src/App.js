e7491a91ea17a0d3fdf5f7599233a67380a5b147
Danielalab <olabolacrayola@hotmail.com>
2021-03-13 14:12:16 -0500

    feat(App): add App structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import UploadImageContainer from './components/UploadImageContainer';
+import ImageLoader from './components/ImageLoader';
+import ImageUploaded from './components/ImageUploaded';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className={styles.h1}>Upload your image</h1>
+      <p className={styles.p}>File should be Jpg, Png ...</p>
+      <UploadImageContainer />
+      <ImageLoader />
+      <ImageUploaded />
     </div>
   );
 }

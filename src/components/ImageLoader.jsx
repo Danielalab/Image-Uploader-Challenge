f6961c1dd7e62652243cd4681f8ddcf91a1f021f
Danielalab <olabolacrayola@hotmail.com>
2021-03-13 14:11:00 -0500

    feat(components): add imageLoader component

---
+++ src/components/ImageLoader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./css/ImageLoader.module.css";
+
+const ImageLoader = () => {
+  return (
+    <section className={styles.section}>
+      <div className="d-flex align-items-center">
+        <span className={"material-icons-round " + styles.iconFileUpload}>
+          upload_file
+        </span>
+        <div className={styles.flexGrow}>
+          <p className={"d-flex " + styles.nameFile}>name file <span>0%</span></p>
+          <div className={styles.loaderBar}></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ImageLoader;

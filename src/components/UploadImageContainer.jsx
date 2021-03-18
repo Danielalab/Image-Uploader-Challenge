02f7d6e92da7f97c3beb8119eb92109a1add6031
Danielalab <olabolacrayola@hotmail.com>
2021-03-18 13:33:01 -0500

    feat(UploadImageContainer): add dragOver & drop event

--- src/components/UploadImageContainer.jsx
+++ src/components/UploadImageContainer.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import styles from './css/UploadImageContainer.module.css';
 
 const UploadImageContainer = () => {
+  const dragOverHandler = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    // Style the drag-and-drop as a "copy file" operation.
+    event.dataTransfer.dropEffect = 'copy';
+  };
+
+  const dropHandler = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files;
+    const fileObject = fileList[0];
+    console.log(fileObject);
+  };
+
+  const changeInputFileHandler = (event) => {
+    const fileList = event.target.files;
+    const fileObject = fileList[0];
+    console.log(fileObject);
+  }
+
   return (
     <>
-      <section className={"secondary-text-color text-center " + styles.section}>
-        <div className={styles.dragAndDrogContainer}>
+      <section className="secondary-text-color text-center ">
+        <div
+          className={styles.dragAndDrogContainer}
+          onDragOver={dragOverHandler}
+          onDrop={dropHandler}
+        >
           <span className={"material-icons " + styles.iconFolderUpload}>
             drive_folder_upload
           </span>
@@ -14,7 +39,11 @@ const UploadImageContainer = () => {
         <p>or</p>
         <button className={styles.buttonWrapper}>
           <label className={styles.labelButton}>Choose a file</label>
-          <input type="file" className={styles.inputFile} />
+          <input
+            type="file"
+            className={styles.inputFile}
+            onChange={changeInputFileHandler}
+          />
         </button>
       </section>
     </>

a3c2892dc49c1051876c4931f091be4a92e379b5
Danielalab <olabolacrayola@hotmail.com>
2021-04-25 17:10:04 -0500

    refactor(api-service): add headers

--- src/controllers/api-service.js
+++ src/controllers/api-service.js
@@ -5,6 +5,10 @@ const saveImageInApi = (file) => {
   return fetch('https://frozen-wildwood-40744.herokuapp.com/images', {
     method: 'POST',
     body: formData,
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': 'https://danielalab.github.io/Image-Uploader-Challenge/',
+    },
   })
     .then((response) => response.json())
     .catch(() => new Error('There was an error, try again'));
